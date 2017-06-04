f44edfa045ebcbdb494fcb1706694aba94568193
developersworkspace@gmail.com <developersworkspace@gmail.com>
2017-06-04 12:13:30 +0200

    update

--- src/app/own-feedstuff-edit/own-feedstuff-edit.component.ts
+++ src/app/own-feedstuff-edit/own-feedstuff-edit.component.ts
@@ -4,13 +4,17 @@ import { ActivatedRoute, Params } from '@angular/router';
 // Services
 import { MainService } from '../services/main.service';
 
+// Imports models
+import { Feedstuff } from './../models/feedstuff';
+import { FeedstuffElement } from './../models/feedstuff-element';
+
 @Component({
   selector: 'app-own-feedstuff-edit',
   templateUrl: './own-feedstuff-edit.component.html',
 })
 export class OwnFeedstuffEditComponent implements OnInit {
 
-  public feedstuff: any = null;
+  public feedstuff: Feedstuff = null;
 
   constructor(private activatedRoute: ActivatedRoute, private mainService: MainService) { }
 
@@ -19,6 +23,11 @@ export class OwnFeedstuffEditComponent implements OnInit {
       const feedstuffId = params['feedstuffId'];
       this.mainService.feedstuffService.findUserFeedstuff(feedstuffId).subscribe((getFeedstuffResult: any) => {
         this.feedstuff = getFeedstuffResult;
+
+        this.feedstuff.elements.sort((a: FeedstuffElement, b: FeedstuffElement) => {
+          return (b.sortOrder < a.sortOrder) ? 1 : -1;
+        });
+
         this.feedstuff.id = feedstuffId;
       });
     });
